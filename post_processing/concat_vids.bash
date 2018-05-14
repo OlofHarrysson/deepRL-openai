3263982f9eb4378900e807aad15a8d05f3f58c46
OlofHarrysson <harrysson.olof@gmail.com>
2018-05-14 18:46:49 +0200

    Added end screen to gif

--- post_processing/concat_vids.bash
+++ post_processing/concat_vids.bash
@@ -20,11 +20,19 @@ for i in ./videos/*.mp4; do
     echo "file $fname" >> $dirn/concat_list.txt
 done
 
+# Do the same thing as above for an end screen
+endscreen=endscreen
+ffmpeg -i $endscreen.png -vf "select=eq(n\,0)" -q:v 3 $dirn/frame_$endscreen.png
+ffmpeg  -loop 1 -i $dirn/frame_$endscreen.png -codec:v h264 -t 2 -pix_fmt yuv420p $dirn/header_$endscreen.mp4
+
+echo "file header_$endscreen.mp4" >> $dirn/concat_list.txt
+
 # concat videos
 ffmpeg -f concat -safe 0 -i $dirn/concat_list.txt -c copy $dirn/output.mp4
 
 ffmpeg -y -i $dirn/output.mp4 -vf fps=50,scale=320:-1:flags=lanczos,palettegen $dirn/palette.png
 
+# Create gif
 ffmpeg -i $dirn/output.mp4 -i $dirn/palette.png -filter_complex "fps=50,scale=320:-1:flags=lanczos[x];[x][1:v]paletteuse" output.gif
 
 cp ./$dirn/output.mp4 .
